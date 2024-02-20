84c7143b3bbb6bf3a889d032ee47ac41002ab318
vashu1 <vashu1@yandex.ru>
2024-02-21 09:05:45 +1100

    secrets example

---
+++ secrets_example.h
@@ -0,0 +1,10 @@
+#ifndef _SECRETS_H_
+#define _SECRETS_H_
+
+#define STASSID "WIFI_NAME"
+#define STAPSK  "WIFI_PASSWORD"
+
+#define OTA_HOSTNAME "power_meter_monitor"
+#define OTA_PASSWORD ""
+
+#endif//_SECRETS_H_
